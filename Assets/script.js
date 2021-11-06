fe1dfc814dcc7340835be5de498b4215110440e6
wisner.maggiel@gmail.com <wisners@Wisnerss-iMac.local>
2021-11-06 14:15:08 -0700

    added styling from bootstrap and luxon for date - not displaying properly

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,34 @@
 
 // Display today's day and date
-var DateTime = luxon.DateTime;
-var todayDate = DateTime.local.weekdayLong
-$("#currentDay").html(todayDate);
+let DateTime = luxon.DateTime;
+let today = DateTime.now();
+let thisHour = DateTime.local().hour;
+// const displayDate = DateTime.DATE_HUGE;
+// const { DateTime } = require("luxon");
+//not getting date to show up
+
+// getDate = () => {
+//     var todayDate = DateTime.DATE_HUGE;
+// };
+
+// //     return today.toLocalString(DateTime.DATE_HUGE);
+// //  
+
+function timeTracker() {
+    //get current number of hours.
+    // var thisHour = DateTime.local().hour;
+    console.log(today);
+    console.log(thisHour);
+    // displayDate = today.getItem(today.utc.DATE_MED);
+    
+    // const today = DateTime;
+    // return today.toLocalString(DateTime.DATE_HUGE);
+};
 
 $(document).ready(function () {
+    timeTracker();
+    // getTime();
+    // getDate();
     // saveBtn click listener 
     $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
@@ -12,24 +36,24 @@ $(document).ready(function () {
 
         // Save text in local storage
         localStorage.setItem(time, text);
-    })
-   
-    function timeTracker() {
-        //get current number of hours.
-        var timeNow = DateTime.local().hour;
-        console.log(timeNow);
+        // getDate();
+        // var todayDate = document.getElementById ("todaysDate");
+        // today.innerHTML(today);
+        // console.log(today);
+        
+        });
 
         // loop over time blocks
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
             // To check the time and add the classes for background indicators
-            if (blockTime < timeNow) {
+            if (blockTime < thisHour) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
             }
-            else if (blockTime === timeNow) {
+            else if (blockTime === thisHour) {
                 $(this).removeClass("past");
                 $(this).removeClass("future");
                 $(this).addClass("present");
@@ -41,7 +65,7 @@ $(document).ready(function () {
 
             }
         })
-    }
+
 
     // Get item from local storage if any
     $("#hour8 .description").val(localStorage.getItem("hour8"));
@@ -55,5 +79,5 @@ $(document).ready(function () {
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-    timeTracker();
+    // timeTracker();
 })
