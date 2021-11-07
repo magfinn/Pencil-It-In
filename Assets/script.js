5e731fc3066423b40b4dfefbb04b80be0cfa57ce
wisner.maggiel@gmail.com <wisners@Wisnerss-iMac.local>
2021-11-07 15:04:47 -0800

    fixed issue with description clearing at the end of the day. Added README description. Ready to deploy to page

--- Assets/script.js
+++ Assets/script.js
@@ -4,47 +4,74 @@ let DateTime = luxon.DateTime;
 let today = DateTime.local();
 let thisHour = DateTime.local().hour;
 
+
 function timeTracker() {
     console.log(thisHour);
     $("#todaysDate").text(today.toLocaleString());
-};
+}
 
+//if it is before 6pm, run function
+function saveEntries() {
+// saveBtn click listener 
+$(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    // Save text in local storage
+    localStorage.setItem(time, text);
+    });
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        // To check the time and add the classes for background indicators
+        if (blockTime < thisHour) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+           
+        }
+        else if (blockTime === thisHour) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    });
+}
+
+//check time && if it is past 5pm, clear. Otherwise, save description in localStorage.
 $(document).ready(function () {
     timeTracker();
-    // saveBtn click listener 
-    $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+    if(thisHour>17) {
+        $(".time-block").each (function(){
+            var text = "";
+            var time = $(this).attr("id");
+            $(this).addClass("past");
 
-        // Save text in local storage
-        localStorage.setItem(time, text);
+            localStorage.setItem(time,text);
+            $("#hour8 .description").val(localStorage.getItem("hour8"));
+            $("#hour9 .description").val(localStorage.getItem("hour9"));
+            $("#hour10 .description").val(localStorage.getItem("hour10"));
+            $("#hour11 .description").val(localStorage.getItem("hour11"));
+            $("#hour12 .description").val(localStorage.getItem("hour12"));
+            $("#hour13 .description").val(localStorage.getItem("hour13"));
+            $("#hour14 .description").val(localStorage.getItem("hour14"));
+            $("#hour15 .description").val(localStorage.getItem("hour15"));
+            $("#hour16 .description").val(localStorage.getItem("hour16"));
+            $("#hour17 .description").val(localStorage.getItem("hour17"));
         });
-
-        // loop over time blocks
-        $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-            // To check the time and add the classes for background indicators
-            if (blockTime < thisHour) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-               
-            }
-            else if (blockTime === thisHour) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-                
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-
-            }
-        })
-
+    } else {
+    saveEntries();
+    }
+});
 
     // Get item from local storage if any
     $("#hour8 .description").val(localStorage.getItem("hour8"));
@@ -57,6 +84,4 @@ $(document).ready(function () {
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
-
-    // timeTracker();
-})
+    
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Work Day Scheduler Starter Code
+<h1>Pencil It In ✍</h1>
+<h2><i>a daily workday planner</i></h2>
+<br><br>
+<h3>Description</h3>
+<p>This planner is for those with an 8-5 or 9-5 schedule who wish to input their tasks for the current day and track their progress throughout the workday. </p>
+<br>
+<br>
+<h3>The Technical Stuff</h3>
+<p>This planner uses Luxon for date/time queries. Starter code included moment.js which is considered a legacy project. <br>
+Each task is saved for the current day. At 6pm, the localStorage is cleaned and the tasks disappear. <br>
+The time is color coordinated:
+<ul>
+<li>grey: time past</li>
+<li>red: current time</li>
+<li>green: future time</li>
+</ul>
+<br>
+<br>
+<h3>Preview:</h3>
+<img src="/assets/Images/Pencil-It-In-preview.png"></img>
+<br>
+<br>
+<h3>Links:</h3>
+<ul>
+<li>Github Repo: https://github.com/magfinn/Pencil-It-In
+
+
